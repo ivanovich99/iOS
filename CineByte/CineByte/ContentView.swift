0dd6f2753d4ba3064583fa6e76a3df9798aab0f8
ivanovich99 <2bavilaoiac@gmail.com>
2025-02-19 21:19:33 -0800

    Hello CineByte

--- CineByte/CineByte/ContentView.swift
+++ CineByte/CineByte/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Helloo, CineByte!")
+            Text("Hellooo, CineByte!")
         }
         .padding()
     }
