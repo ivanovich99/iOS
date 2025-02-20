db8ec4adb89c68cea8943268f27564673968aa77
dnuno10 <145091597+dnuno10@users.noreply.github.com>
2025-02-19 20:52:19 -0800

    Add files via upload

---
+++ cineByte/cineByte/cineByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  cineByteApp.swift
+//  cineByte
+//
+//  Created by Daniel Nuno on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct cineByteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cineByte/cineByteTests/cineByteTests.swift
@@ -0,0 +1,17 @@
+//
+//  cineByteTests.swift
+//  cineByteTests
+//
+//  Created by Daniel Nuno on 2/19/25.
+//
+
+import Testing
+@testable import cineByte
+
+struct cineByteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
