4c114fd1b10efb0581285f2661096d93f2fa7430
CETYS Universidad <curso@MAC-LAB.local>
2025-02-19 20:43:49 -0800

    first commit

---
+++ CineByte/CineByte/CineByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineByteApp.swift
+//  CineByte
+//
+//  Created by CETYS Universidad  on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CineByteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineByte/CineByteTests/CineByteTests.swift
@@ -0,0 +1,17 @@
+//
+//  CineByteTests.swift
+//  CineByteTests
+//
+//  Created by CETYS Universidad  on 19/02/25.
+//
+
+import Testing
+@testable import CineByte
+
+struct CineByteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
