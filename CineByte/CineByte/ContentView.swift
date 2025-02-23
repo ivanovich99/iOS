e03306aa04f90c990ad2d19cd0b48f5e213a0720
dnuno10 <dnuno@cetys.edu.mx>
2025-02-22 19:02:15 -0800

    User & Movie Logic Implementation

---
+++ CineByte/CineByte/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineByte
+//
+//  Created by CETYS Universidad  on 19/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hellooaaaooojunuihh, CineByte!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
